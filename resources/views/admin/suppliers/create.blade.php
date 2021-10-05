9f12a5295c7563bf7dab6c62e1b254d99714be95
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-05 21:36:43 +0000

    supplier views

---
+++ resources/views/admin/suppliers/create.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@push('page-css')
+<link href="{{asset('assets/extra-libs/prism/prism.css')}}" rel="stylesheet">
+@endpush
+
+@section('content')
+<div class="row">
+    <div class="col s12 l12">
+        <div class="card">
+            <div class="card-content">
+                <h5 class="card-title activator">Create New Supplier </h5>
+                <form method="post" action="{{route('suppliers.store')}}">
+                    @csrf
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input id="supplier"value="{{old('name')}}" name="name" type="text" required>
+                            <label for="supplier">Name</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input id="phone" value="{{old('phone')}}" name="phone" type="text" >
+                            <label for="phone">Phone</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input id="email" value="{{old('email')}}" name="email" type="email">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input id="address" value="{{old('address')}}" name="address" type="text">
+                            <label for="address">Address</label>
+                        </div>
+                    </div>
+                    
+                    <div class="row form-repeater">
+                        <div data-repeater-list="products">
+                            <div data-repeater-item>
+                                <div class="input-field col s10">
+                                    <input id="product" type="text" name="name">
+                                    <label for="product">Product</label>
+                                </div>
+                                <div class="input-field col s2">
+                                    <button data-repeater-delete="" data-position="top" data-delay="50" data-tooltip="Remove" class="btn red waves-effect waves-light tooltipped" type="button"><i class="material-icons">clear</i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" data-repeater-create="" class="btn teal waves-effect waves-light m-l-10">Add Product
+                        </button>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea name="comment" value="{{old('comment')}}" id="comment" cols="30" rows="15"></textarea>
+                            <label for="comment">Comment</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <button class="btn green waves-effect waves-light " type="submit">Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</div>
+@endsection
+
+@push('page-js')
+<script src="{{asset('assets/extra-libs/prism/prism.js')}}"></script>
+<script src="{{asset('dist/js/pages/forms/jquery.validate.min.js')}}"></script>
+@endpush
---
+++ resources/views/admin/suppliers/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@push('page-css')
+    <link rel="stylesheet" href="{{asset('dist/css/pages/data-table.css')}}">
+@endpush
+
+@section('content')
+<div class="row">
+    <div class="col s12">
+        <div class="card">
+            <div class="card-content">
+                <a href="{{route('suppliers.create')}}"><button class="btn btn-primary right">Create Supplier</button></a>
+                <h5 class="card-title">Suppliers</h5>
+                <table id="file_export" class="table table-striped table-bordered display">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                            <th>Products</th>
+                            <th>Comment</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        @foreach ($suppliers as $supplier)
+                        <tr>
+                            <td>{{$supplier->name}}</td>
+                            <td>{{$supplier->email}}</td>
+                            <td>{{$supplier->phone}}</td>
+                            <td>{{$supplier->address}}</td>
+                            <td>
+                                @foreach ($supplier->products as $product)
+                                    <span>{{implode(",",$product)}}</span>
+                                @endforeach
+                            </td>
+                            <td>{{$supplier->comment}}</td>
+                            <td>
+                                <a href="{{route('suppliers.edit',$supplier)}}"><button class="btn tooltipped" data-position="top" data-delay="50" data-tooltip="Edit"><i class="material-icons">edit</i></button></a>
+                                <br><br>
+                                <form action="{{route('suppliers.destroy',$supplier)}}" method="post">
+                                    @csrf
+                                    @method("DELETE")
+                                    <button class="btn red tooltipped" type="submit" data-position="top" data-delay="50" data-tooltip="Delete"><i class="material-icons">delete</i></button>
+                                </form>
+
+                            </td>
+                        </tr>
+                        @endforeach
+                       
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('page-js')
+<script src="{{asset('assets/extra-libs/DataTables/datatables.min.js')}}"></script>
+<script src="{{asset('dist/js/pages/datatable/datatable-advanced.init.js')}}"></script>
+@endpush
