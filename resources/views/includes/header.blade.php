f76164938e67bde5a25fe3168b73675fc9ff6c7c
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-09 22:45:27 +0000

    updated

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -9,12 +9,12 @@
             <!-- ============================================================== -->
             <a href="javascript:void(0)" class="brand-logo">
                 <span class="icon">
-                    <img class="light-logo" src="../../assets/images/logo-light-icon.png">
-                    <img class="dark-logo" src="../../assets/images/logo-icon.png">
+                    <img class="light-logo" src="{{asset('assets/images/logo-light-icon.png')}}">
+                    <img class="dark-logo" src="{{asset('assets/images/logo-icon.png')}}">
                 </span>
                 <span class="text">
-                    <img class="light-logo" src="../../assets/images/logo-light-text.png">
-                    <img class="dark-logo" src="../../assets/images/logo-text.png">
+                    <img class="light-logo" src="{{asset('assets/images/logo-light-text.png')}}">
+                    <img class="dark-logo" src="{{asset('assets/images/logo-text.png')}}">
                 </span>
             </a>
             <!-- ============================================================== -->
