2ac8e35e0a50acdf3e8e6bed6529201e1601fdf7
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-10 17:15:06 +0000

    permission checks

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -8,51 +8,64 @@
                         <span class="hide-menu">Dashboard</span>
                     </a>
                 </li>
-
+                @can('view-categories')
                 <li>
                     <a href="{{route('categories.index')}}" class="collapsible-header">
                         <i class="material-icons">assignment</i>
                         <span class="hide-menu">Categories</span>
                     </a>
                 </li>
-
+                @endcan
+                @can('view-suppliers')
                 <li>
                     <a href="{{route('suppliers.index')}}" class="collapsible-header">
                         <i class="material-icons">group</i>
                         <span class="hide-menu">Suppliers</span>
                     </a>
                 </li>
+                @endcan
+                @can('view-purchases')
                 <li>
                     <a href="{{route('purchases.index')}}" class="collapsible-header">
                         <i class="material-icons">add_shopping_cart</i>
                         <span class="hide-menu">Purchases</span>
                     </a>
                 </li>
+                @endcan
+                @can('view-products')
                 <li>
                     <a href="{{route('products.index')}}" class="collapsible-header">
                         <i class="material-icons">business_center</i>
                         <span class="hide-menu">Products</span>
                     </a>
                 </li>
+                @endcan
+                @can('view-sales')
                 <li>
                     <a href="{{route('sales.index')}}" class="collapsible-header">
                         <i class="material-icons">equalizer</i>
                         <span class="hide-menu">Sales</span>
                     </a>
                 </li>
+                @endcan
+                @can('view-customers')
                 <li>
                     <a href="{{route('customers.index')}}" class="collapsible-header">
                         <i class="material-icons">group</i>
                         <span class="hide-menu">Customers</span>
                     </a>
                 </li>
-                <li>
-                    <a href="{{route('backup.index')}}" class="collapsible-header">
-                        <i class="material-icons">backup</i>
-                        <span class="hide-menu">Backups</span>
-                    </a>
-                </li>
-
+                @endcan
+                @can('view-backups')
+                  <li>
+                        <a href="{{route('backup.index')}}" class="collapsible-header">
+                            <i class="material-icons">backup</i>
+                            <span class="hide-menu">Backups</span>
+                        </a>
+                    </li>  
+                @endcan
+                
+                @can('access-control')
                 <li>
                     <a class="collapsible-header has-arrow"><i class="material-icons">verified_user</i><span class="hide-menu">Authentication</span></a>
                     <div class="collapsible-body">
@@ -63,73 +76,26 @@
                                     <span class="hide-menu">Users</span>
                                 </a>
                             </li>
+                            @can('view-roles')
                             <li>
                                 <a href="{{route('roles.index')}}">
                                     <i class="material-icons">person</i>
                                     <span class="hide-menu">Roles</span>
                                 </a>
                             </li>
+                            @endcan
+                            @can('view-permissions')
                             <li>
                                 <a href="{{route('permissions.index')}}">
                                     <i class="material-icons">person</i>
                                     <span class="hide-menu">Permissions</span>
                                 </a>
                             </li>
+                            @endcan
                         </ul>
                     </div>
                 </li>
-
-                <li>
-                    <a class="collapsible-header has-arrow"><i class="material-icons">clear_all</i><span class="hide-menu">Multi Levels</span></a>
-                    <div class="collapsible-body">
-                        <ul class="collapsible" data-collapsible="accordion">
-                            <li>
-                                <a href="javascript:void(0)">
-                                    <i class="material-icons">grade</i>
-                                    <span class="hide-menu">Second level</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a class="collapsible-header has-arrow">
-                                    <i class="material-icons">looks_two</i>
-                                    <span class="nav-text">Second level child</span>
-                                </a>
-                                <div class="collapsible-body">
-                                    <ul class="collapsible" data-collapsible="accordion">
-                                        <li>
-                                            <a href="javascript:void(0)">
-                                                <i class="material-icons">grade</i>
-                                                <span class="hide-menu">Third level</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a class="collapsible-header has-arrow">
-                                                <i class="material-icons">looks_3</i>
-                                                <span class="hide-menu">Third level child</span>
-                                            </a>
-                                            <div class="collapsible-body">
-                                                <ul class="collapsible" data-collapsible="accordion">
-                                                    <li>
-                                                        <a href="javascript:void(0)">
-                                                            <i class="material-icons">grade</i>
-                                                            <span class="hide-menu">Forth level</span>
-                                                        </a>
-                                                    </li>
-                                                    <li>
-                                                        <a href="javascript:void(0)">
-                                                            <i class="material-icons">grade</i>
-                                                            <span class="hide-menu">Forth level</span>
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
+                @endcan
                 <li>
                     <form action="{{route('logout')}}" method="post">
                         @csrf
