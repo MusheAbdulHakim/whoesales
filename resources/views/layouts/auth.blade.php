fc0839b64a93123eb9565f771193e7907d824530
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-10 17:10:39 +0000

    csrf meta tag

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/favicon.png')}}">
     <title>{{ucfirst(config('app.name'))}} - {{ucwords($title)?? ''}}</title>
     <link href="{{asset('dist/css/style.css')}}" rel="stylesheet">
